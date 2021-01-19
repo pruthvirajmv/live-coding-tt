b57b5d5677532e1bca58089f2aa047f395ece0f0
Pruthviraj-MV <Pruthviraj-MV@code.github.com>
2021-01-19 23:50:51 +0530

    Create twoSumSorted.js

---
+++ Week 2/twoSumSorted.js
@@ -0,0 +1,11 @@
+var twoSum = function(numbers, target) {
+    
+    for(let i=0, j=numbers.length-1;i<j;){
+        if(numbers[i] == target - numbers[j]) return [i+1,j+1];
+        else if (numbers[i] < target - numbers[j]) j--;
+        else i++;     
+    }
+    
+};
+
+console.log(twoSum([4,3,2,5,7],6)); 
